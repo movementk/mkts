4f9ac623927491d7d88952137ac145f49afccfce
kyh5392 <kyh90495392@gmail.com>
2017-02-09 19:06:05 +0900

    main layout 코딩 작업중

--- index.php
+++ index.php
@@ -5,28 +5,103 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>명광통상(주)</title>
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/css/default.css" rel="stylesheet">
+    <link href="/assets/css/main.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     </head>
-    <body id="content">
-<!--
+    <body class="main">
         <a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
         <a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
--->
         <header id="header">
-            1
+            <div class="container">
+                <nav id="gnb">
+                        <h1 class="logo"><a href="/"><img src="/assets/images/logo.jpg" alt="로고"></a></h1>
+                        <ul class="gnb-menu">
+                            <li><a href="#">회사소개</a></li>
+                            <li><a href="#">생산</a></li>
+                            <li><a href="#">견적의뢰</a></li>
+                            <li><a href="#">공지사항</a></li>
+                        </ul>
+                        <div class="quick-menu">
+                            <ul>
+                                <li><a href="#">HOME</a></li>
+                                <li><a href="#">LOCATION</a></li>
+                            </ul>
+                        </div>
+                </nav>
+            </div>
         </header>
         <main id="content">
-            2
+            <div class="container-fluid" style="height: 870px;">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <div class="visual-slider">
+                            <ul>
+                                <li>슬라이더 부분</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <div class="layout-prame">
+                            <div class="product">
+                                <h2>PRODUCT</h2><br>
+                                <p><b>명광통상(주)</b>의 주요제품현황 입니다.</p>
+                            </div>
+                            <div class="counsel">
+                                <h2>COUNSEL</h2><br>
+                                <p>명광통상(주)에<br>제품 견적을 의뢰하실수 있습니다.</p>
+                            </div>
+                        </div>
+                        <div class="layout-prame">
+                            <div class="location">
+                                <h2>LOCATION</h2><br>
+                                <p>031.403.8724</p>
+                            </div>
+                            <div class="map">
+                                지도영역
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
         <footer id="footer">
-            3
+            <div class="policy">
+                <div class="container">
+                    <ul>
+                       <li class="active">
+                           <a href="#">회사소개</a>
+                       </li>
+                       <li>
+                           <a href="#">이용약관</a>
+                       </li>
+                       <li>
+                           <a href="#">개인정보취급방침</a>
+                       </li>
+                       <li>
+                           <a href="#">이메일무단수집거부</a>
+                       </li>
+                       <li>
+                           <a href="#">CONTACT US</a>
+                       </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="company">
+                <div class="container">
+                    명광통상(주) 경기도 안산시 단원구 신원로 66(성곡동)  ㅣ  전화번호 : 031-403-8724  ㅣ  팩스번호 : 031-403-8668<br>
+                    대표이사 : 윤영구 ㅣ  사업자등록번호 : 111-81-36198<br><br>
+
+                    (c) 2016 myungkwang co.,LTD  all right reserved. <br>
+                    site by movementk group
+                </div>
+            </div>
         </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
     </body>
 </html>
